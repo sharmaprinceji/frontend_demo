7c2be5c98ff386522d7c53b4cb0023c6ac4e37ae
sharmajiiprince <prince.raj.cs19@ggits.net>
2024-01-08 00:16:15 +0530

    counter section

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ function getdata(){
 
 // console.log("multiplication of x and y is :",result);
 
+ const arr=[10,20,30,40,50,60,70,80,90,100];
+// for(let i=0;i<arr.length;i++){
+//   if(arr[i]%6==0){
+//     console.log('elements of loop:',arr[i]);
+//    }
+// }
+
+// arr.forEach((data)=>{
+//  if(data%6==0){
+//   console.log('elements of for each:',data);
+//  }
+// });
+
+// const ans=arr.map((data)=>{
+//   if(data%6==0){
+//      return data;
+//    }
+// });
+// console.log(ans);
+
+// function myFunction(value) {
+//   return value%6==0;
+// }
+// const res=arr.filter(myFunction);
+// console.log(res);
+
+let sum = arr.reduce(myFunction);
+
+function myFunction(total, value) {
+  return total + value;
+}
+console.log(sum);
