3dc86ae0b117d385de888cf565f10b3e37232167
sharmajiiprince <prince.raj.cs19@ggits.net>
2024-01-11 21:45:04 +0530

    recipt

--- practise.js
+++ practise.js
@@ -27,3 +27,60 @@
 //      console.log(y)
 // })();
 
+//6
+const arr=[12,23,11,34,72,45,90];
+let maxi=0;
+// arr.forEach((data)=>{
+//   if(maxi<data){
+//     maxi=data;
+//   }
+// })
+
+// arr.map((data)=>{
+//     if(data>maxi){
+//         maxi=data;
+//     }
+// })
+//console.log(maxi);
+
+//7.two pointer algorithm.
+let arr1=[11,12,13,14,15,17];
+let arr2=[1,5,7,9,10,18];
+let ans=[]
+let i=0,j=0;
+while(i<arr1.length && j<arr2.length){
+    if(arr1[i]>arr2[j]){
+        ans.push(arr2[j]);
+        j++;
+    }
+    else{
+        ans.push(arr1[i]);
+        i++;
+    }
+}
+
+while(i<arr1.length){
+    ans.push(arr1[i]);
+    i++;
+}
+
+while(j<arr2.length){
+    ans.push(arr2[j]);
+    j++;
+}
+
+//console.log(ans);
+//8.
+// const str="mom";
+// (function(){
+//     const temp=str.split("").reverse().join("");
+//     if(temp==str){
+//         console.log('string is palindrom!');
+//     }
+//     else{
+//         console.log('string is not palindrom!');
+//     }
+// })();
+
+
+
