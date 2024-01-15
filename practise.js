e4349a7ee3a2c15cdf80535a92f7c3c4a315452d
sharmajiiprince <prince.raj.cs19@ggits.net>
2024-01-16 00:43:19 +0530

    dsa proplem in js

--- practise.js
+++ practise.js
@@ -29,7 +29,7 @@
 
 //6
 const arr=[12,23,11,34,72,45,90];
-let maxi=0;
+//let maxi=0;
 // arr.forEach((data)=>{
 //   if(maxi<data){
 //     maxi=data;
@@ -81,6 +81,48 @@ while(j<arr2.length){
 //         console.log('string is not palindrom!');
 //     }
 // })();
+
+
+//find kth no of maximum frequent elements. or Top K Frequent Elements
+let num=[1,3,2,2,1,3,2,3,1,1,3,4,5,1,2,2,2,2,3,3,3,3];   //1->5, 2->3, 3->4, 4->1, 5->1 //1,3,2,4,5
+let k=3;        
+// let maxi = 0;
+// num.forEach((data) => {                        //time=n+n+(k*n)==2n+kn==> 2n+n^2=>  0(n^2)  space=o(n) wrost time complexity -->brute force approach.
+//   if (maxi < data) {
+//     maxi = data;
+//   }
+// });
+// //var count = new Array(maxi + 1).fill(0);
+// for (let i = 0; i < num.length; i++) {
+//   count[num[i]]++;
+// }
+// //console.log(count)
+// while (k--) {
+//   let temp = count[0],
+//     idx = 0;
+//   for (let i = 0; i < count.length; i++) {
+//     if (temp < count[i]) {
+//       temp = count[i]; 
+//       idx = i;
+//     }
+//   }
+//   count[idx] = 0;
+//   //console.log(idx);
+// }
+
+const count = new Map();
+num.forEach((data) => {
+  count.set(data, (count.get(data) || 0) + 1);
+});
+//console.log(count)
+const sortedCount = [...count.entries()].sort((a,b)=>b[1]-a[1]); //nlogn->time optimize approach
+//console.log(sortedCount);
+for (let i = 0; i < k && i < sortedCount.length; i++){
+  console.log(sortedCount[i][0]);
+}
+
+
+
 
 
 
